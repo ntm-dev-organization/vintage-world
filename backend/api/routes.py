9e04ab8e96a4f1c0124bfd48c5f5a1d2134c524d
martimfm1 <mmartim227@gmail.com>
2025-05-14 15:36:56 +0100

    index done

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -11,6 +11,14 @@ routes = Blueprint('routes', __name__)
 async def index():
     return await render_template("index.html")
 
+@routes.route("/resell")
+async def resell():
+    return await render_template("resell.html")
+
+@routes.route("/yourself")
+async def yourself():
+    return await render_template("yourself.html")
+
 @routes.route('/frontend/static/<path:filename>')
 async def static_files(filename):
     return await send_from_directory('frontend/static', filename)
